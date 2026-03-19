9c9d42d0058545915e6da5b61aa4e4848a6ca105
AnzinFelipe <ian.felipe.drunen@gmail.com>
2026-03-19 20:14:10 -0300

    Adicionando tarefas na lista encadeada

--- source/rate.c
+++ source/rate.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct Atividade {
+    char nome[100];
+    float periodo;
+    float burst;
+    struct Atividade *prox;
+} Atividade;
+
+void adicionar_atividade(Atividade **head, char nome[100], float periodo, float burst);
+void printar_atividades(Atividade *head);
 
 int main (int argc, char *argv[]) {
     FILE *arquivo = fopen(argv[1], "r");
@@ -8,6 +19,7 @@ int main (int argc, char *argv[]) {
         exit(1);
     }
 
+    Atividade *atividade = NULL;
     char nome[100];
     float tempo_total, periodo, burst;
     int i = 0;
@@ -20,11 +32,40 @@ int main (int argc, char *argv[]) {
             if (fscanf(arquivo, "%s %f %f", nome, &periodo, &burst) == EOF) {
                 break;
             }
+            adicionar_atividade(&atividade, nome, periodo, burst);
             printf("%s %1.f %1.f\n", nome, periodo, burst);
         }
     }
+    printf("Atividades na lista encadeada:\n");
+    printar_atividades(atividade);
     
     fclose(arquivo);
 
     return 0;
+}
+
+void adicionar_atividade(Atividade **head, char nome[100], float periodo, float burst) {
+    Atividade *novo = (Atividade *)malloc(sizeof(Atividade));
+    if (novo != NULL) {
+        strcpy(novo->nome, nome);
+        novo->periodo = periodo;
+        novo->burst = burst;
+        novo->prox = NULL;
+        if (*head == NULL) {
+            *head = novo;
+        } else {
+            Atividade *aux = *head;
+            while(aux->prox != NULL) {
+                aux = aux->prox;
+            }
+            aux->prox = novo;
+        }
+    }
+}
+
+void printar_atividades(Atividade *head) {
+    while(head != NULL) {
+        printf("%s %1.f %1.f\n", head->nome, head->periodo, head->burst);
+        head = head->prox;
+    }
 }
